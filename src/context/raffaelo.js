76afd73a59cf1f17ab89f3a67a6e645aa23c7496
Ashish Augustine <ashish.augustine123@gmail.com>
2023-05-18 13:00:34 +0200

    Update raffaelo.js

--- src/context/raffaelo.js
+++ src/context/raffaelo.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useContext, useReducer } from 'react'
 import reducer from '../reducers/filter_reducer'
 import {
-  LOAD_PRODUCTS,
-  SET_GRIDVIEW,
-  SET_LISTVIEW,
-  UPDATE_SORT,
   SORT_PRODUCTS,
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
+  LOAD_PRODUCTS,
+  SET_GRIDVIEW,
+  SET_LISTVIEW,
+  UPDATE_SORT
 } from '../actions'
 import { useProductsContext } from './products_context'
 
